7d9a464db680e82b58cd93390b31532146b9d174
Jules Roche <julesro42@gmail.com>
2024-09-30 12:23:24 +0200

    first method CategoryController

--- server/src/Controller/CategoryController.php
+++ server/src/Controller/CategoryController.php
@@ -1,13 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
-class CategoryController
+class CategoryController extends AbstractController
 {
-    public function index()
+    #[Route(path: '/category', name: 'category')]
+    public function index() : Response
     {
-        return 'CategoryController';
+        return $this->render('category.html.twig');
     }
+
 }
