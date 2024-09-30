1cd0939490ba887663d7cc9d07d7dd9031f43176
Jules Roche <julesro42@gmail.com>
2024-09-30 12:20:47 +0200

    refacto controller

--- server/src/Controller/MediaController.php
+++ server/src/Controller/MediaController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MovieController extends AbstractController
+class MediaController extends AbstractController
 {
     #[Route(path: '/movie/{name}', name: 'movie_show')]
     public function index(string $name) : Response
