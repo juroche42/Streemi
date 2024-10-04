2b5198e0b1a2bc30ce45f1fe129574e482869db8
Jules Roche <julesro42@gmail.com>
2024-10-04 09:39:41 +0200

    add Entity WatchHistory

---
+++ server/src/Entity/WatchHistory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WatchHistoryRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WatchHistoryRepository::class)]
+class WatchHistory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $lastWatchedAt = null;
+
+    #[ORM\Column]
+    private ?int $numberOfViews = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastWatchedAt(): ?\DateTimeInterface
+    {
+        return $this->lastWatchedAt;
+    }
+
+    public function setLastWatchedAt(\DateTimeInterface $lastWatchedAt): static
+    {
+        $this->lastWatchedAt = $lastWatchedAt;
+
+        return $this;
+    }
+
+    public function getNumberOfViews(): ?int
+    {
+        return $this->numberOfViews;
+    }
+
+    public function setNumberOfViews(int $numberOfViews): static
+    {
+        $this->numberOfViews = $numberOfViews;
+
+        return $this;
+    }
+}
