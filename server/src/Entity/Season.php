58a58484e3c42b502a9d6b95c3ff342cd44a6bb6
Jules Roche <julesro42@gmail.com>
2024-10-04 10:33:48 +0200

    add entity Season

---
+++ server/src/Entity/Season.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SeasonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SeasonRepository::class)]
+class Season
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $seasonNumber = null;
+
+    #[ORM\ManyToOne(inversedBy: 'seasons')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Serie $serie = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSeasonNumber(): ?int
+    {
+        return $this->seasonNumber;
+    }
+
+    public function setSeasonNumber(int $seasonNumber): static
+    {
+        $this->seasonNumber = $seasonNumber;
+
+        return $this;
+    }
+
+    public function getSerie(): ?Serie
+    {
+        return $this->serie;
+    }
+
+    public function setSerie(?Serie $serie): static
+    {
+        $this->serie = $serie;
+
+        return $this;
+    }
+}
