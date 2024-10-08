0dc81157783497376cde57a716cd36710e16a3e3
Jules Roche <julesro42@gmail.com>
2024-10-08 16:40:04 +0200

    add data inside subscription card

---
+++ server/src/Controller/SubscriptionController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\SubscriptionRepository;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SubscriptionController extends AbstractController
+{
+    #[Route(path: '/subscription', name: 'subscription')]
+    public function index(SubscriptionRepository $subscriptionRepository) : Response
+    {
+        $subscriptions = $subscriptionRepository->findAll();
+        dump($subscriptions);
+        return $this->render('abonnements.html.twig',
+            ['subscriptions' => $subscriptions]
+        );
+    }
+
+}
