8d9bdf479fb8584ebe525665e271cd88c774ac7f
Jules Roche <julesro42@gmail.com>
2024-12-13 15:59:07 +0100

    send email

--- server/src/Controller/AuthController.php
+++ server/src/Controller/AuthController.php
@@ -6,7 +6,9 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,7 +21,7 @@ class AuthController extends AbstractController
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        dump($error);
+
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
@@ -35,11 +37,25 @@ class AuthController extends AbstractController
         return $this->render('register.html.twig');
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route(path: '/forgot-password', name: 'forgot_password')]
     public function forgotPassword(Request $request, UserRepository $userRepository, MailerInterface $mailer) : Response
     {
         $email = $request->get('email');
+
         $user = $userRepository->findOneBy(['email' => $email]);
+
+        if ($user) {
+            $email = (new Email())
+                ->from('test@example.com')
+                ->to($user->getEmail())
+                ->subject('Reset Password')
+                ->text('Click here to reset your password')
+                ->html('<a href="http://localhost:8000/reset-password">Click here to reset your password</a>');
+            $mailer->send($email);
+        }
         return $this->render('forgot.html.twig');
     }
 
@@ -50,8 +66,11 @@ class AuthController extends AbstractController
     }
 
     #[Route(path: '/reset-password', name: 'reset-password')]
-    public function resetPassword() : Response
+    public function resetPassword(Request $request) : Response
     {
+        $password = $request->get('password');
+
+        
         return $this->render('reset.html.twig');
     }
 
