cd7e051fbf165aa75048a4f2224962acf7e21e93
Jules Roche <julesro42@gmail.com>
2024-12-13 13:55:47 +0100

    Création du AuthController

--- server/src/Controller/AuthController.php
+++ server/src/Controller/AuthController.php
@@ -7,13 +7,23 @@ namespace App\Controller;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController
 {
     #[Route(path: '/login', name: 'login')]
-    public function index() : Response
+    public function index(AuthenticationUtils $authenticationUtils) : Response
     {
-        return $this->render('login.html.twig');
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 
     #[Route(path: '/register', name: 'register')]
